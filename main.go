3baa3a05632bc2b2bc45d71ee7ac5c03cb725a55
Nana Osei Yeboah <yeboahnanaosei@gmail.com>
2020-03-02 22:55:31 +0000

    Use os.Args not commandline flags

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/json"
-	"flag"
 	"fmt"
 	"os"
 
@@ -10,33 +9,28 @@ import (
 )
 
 func main() {
-	out := make(map[string]interface{})
+	response := make(map[string]interface{})
 
-	csvFilepath := flag.String("file", "", "The path to the file the CSV file")
-	flag.Parse()
-
-	if *csvFilepath == "" {
-		out["success"] = false
-		out["code"] = 400
-		out["summary"] = "Invalid request"
-		out["error"] = map[string]string{
-			"msg": "No path to csv file supplied. A path to a csv file was expected but none was supplied",
-			"fix": "use the '-file' flag to supply the path eg. -file /path/to/csv/file",
+	if len(os.Args) != 2 {
+		response["success"] = false
+		response["summary"] = "Exactly one argument expected"
+		response["error"] = map[string]string{
+			"msg": "cvet expects exactly one argument which is the path to the csv file being vetted",
+			"fix": fmt.Sprintf("call cvet with the path to the csv file as the first argument. Eg %s /path/to/csv/file", os.Args[0]),
 		}
-		json.NewEncoder(os.Stdout).Encode(out)
+		json.NewEncoder(os.Stdout).Encode(response)
 		return
 	}
 
-	csvFile, err := os.Open(*csvFilepath)
+	csvFile, err := os.Open(os.Args[1])
 	if err != nil {
-		out["success"] = false
-		out["code"] = 500
-		out["summary"] = "An internal error occured"
-		out["error"] = map[string]string{
-			"msg": fmt.Sprintf("There was an error trying to process the csv file. Server said: %v", err),
-			"fix": "Ensure you provided a valid csv file. If this continues, please wait and try again later. You can also contact support",
+		response["success"] = false
+		response["summary"] = "An internal error occured"
+		response["error"] = map[string]string{
+			"msg": fmt.Sprintf("There was an error trying to process the csv file: %v", err),
+			"fix": "Ensure you provided a valid csv file.",
 		}
-		json.NewEncoder(os.Stdout).Encode(out)
+		json.NewEncoder(os.Stdout).Encode(response)
 		return
 	}
 	defer csvFile.Close()
@@ -44,24 +38,21 @@ func main() {
 	validRecords, invalidRecords, err := csv.Validate(csvFile)
 
 	if err != nil {
-		out["success"] = false
-		out["code"] = 500
-		out["summary"] = "An internal error occured"
-		out["error"] = map[string]string{
-			"msg": fmt.Sprintf("There was an error trying to process the csv file. Server said: %v", err),
+		response["success"] = false
+		response["summary"] = "An internal error occured"
+		response["error"] = map[string]string{
+			"msg": fmt.Sprintf("There was an error trying to process the csv file: %v", err),
 			"fix": "Ensure you provided a valid csv file. If this continues, please wait and try again later. You can also contact support",
 		}
-		json.NewEncoder(os.Stdout).Encode(out)
+		json.NewEncoder(os.Stdout).Encode(response)
 		return
 	}
 
-	out["success"] = true
-	out["code"] = 200
-	out["summary"] = "Operation successful"
-	out["data"] = map[string]interface{}{
+	response["success"] = true
+	response["summary"] = "File vetted successfully"
+	response["data"] = map[string]interface{}{
 		"validRecords":   validRecords,
 		"invalidRecords": invalidRecords,
 	}
-	json.NewEncoder(os.Stdout).Encode(out)
-	return
+	json.NewEncoder(os.Stdout).Encode(response)
 }
