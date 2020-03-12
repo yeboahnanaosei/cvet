6b6386f5c518bca560ff021bd21e07097aed6deb
Nana Osei Yeboah <yeboahnanaosei@gmail.com>
2020-03-12 09:03:21 +0000

    Remove csv dependency
    The custom csv package only exported one function
    We've brought that function directly into cvet

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module cvet
 
 go 1.14
-
-require github.com/yeboahnanaosei/gitplus v1.2.0
--- main.go
+++ main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
+	"encoding/csv"
 	"encoding/json"
 	"fmt"
+	"io"
 	"os"
-
-	"github.com/yeboahnanaosei/gitplus/csv"
+	"strings"
 )
 
 func main() {
@@ -35,7 +36,7 @@ func main() {
 	}
 	defer csvFile.Close()
 
-	validRecords, invalidRecords, err := csv.Validate(csvFile)
+	validRecords, invalidRecords, err := parse(csvFile)
 
 	if err != nil {
 		response["success"] = false
@@ -55,4 +56,45 @@ func main() {
 		"invalidRecords": invalidRecords,
 	}
 	json.NewEncoder(os.Stdout).Encode(response)
+}
+
+// InvalidRecord is a record that is not valid.
+type InvalidRecord struct {
+	RowNumber int      `json:"row"`
+	Columns   []string `json:"columns"`
+}
+
+// parse validates f as a valid csv file with valid data.
+func parse(f io.Reader) (validRecords [][]string, invalidRecords []InvalidRecord, err error) {
+	r := csv.NewReader(f)
+	r.TrimLeadingSpace = true
+
+	uploadedCSV, err := r.ReadAll()
+	if err != nil {
+		return validRecords, invalidRecords, err
+	}
+
+	rowOffset := 2
+	header := uploadedCSV[0]
+	headerLength := len(header)
+
+	for row, record := range uploadedCSV[1:] {
+		currentRecord := new(InvalidRecord)
+		currentRecord.RowNumber = row + rowOffset
+		recordIsValid := true
+
+		for column, field := range record {
+			if strings.TrimSpace(field) == "" {
+				recordIsValid = false
+				currentRecord.Columns = append(currentRecord.Columns, header[column])
+			}
+		}
+
+		if recordIsValid {
+			validRecords = append(validRecords, record)
+		} else if !recordIsValid && len(currentRecord.Columns) != headerLength {
+			invalidRecords = append(invalidRecords, *currentRecord)
+		}
+	}
+	return validRecords, invalidRecords, nil
 }
