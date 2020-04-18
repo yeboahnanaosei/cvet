bbf53f70b9ea5f04a7dcc4eaa61ee0d378b5a912
Nana Osei Yeboah <yeboahnanaosei@gmail.com>
2020-04-18 08:45:50 +0000

    Use cval package

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/yeboahnanaosei/cvet
 
 go 1.14
+
+require github.com/yeboahnanaosei/go v1.2.9
--- main.go
+++ main.go
@@ -1,12 +1,11 @@
 package main
 
 import (
-	"encoding/csv"
 	"encoding/json"
 	"fmt"
-	"io"
 	"os"
-	"strings"
+
+	"github.com/yeboahnanaosei/go/cval"
 )
 
 type jsonResponse struct {
@@ -45,7 +44,7 @@ func main() {
 	defer csvFile.Close()
 
 	// Perform the actual parsing of the csv file
-	validRecords, invalidRecords, err := parse(csvFile)
+	validRecords, invalidRecords, err := cval.Validate(csvFile)
 
 	if err != nil {
 		payload.Success = false
@@ -65,53 +64,4 @@ func main() {
 		"invalidRecords": invalidRecords,
 	}
 	json.NewEncoder(os.Stdout).Encode(payload)
-}
-
-// invalidRecord is a record or row in the csv file that has at least
-// one empty column.
-type invalidRecord struct {
-	RowNumber int      `json:"row"`
-	Columns   []string `json:"cols"`
-}
-
-// parse validates f as a valid csv file with valid data.
-func parse(f io.Reader) (validRecords [][]string, invalidRecords []invalidRecord, err error) {
-	r := csv.NewReader(f)
-	r.TrimLeadingSpace = true
-
-	uploadedCSV, err := r.ReadAll()
-	if err != nil {
-		return validRecords, invalidRecords, err
-	}
-
-	// The first row in the csv is usually the header. Which has the name of each
-	// column in the csv file
-	var header []string = uploadedCSV[0]
-	headerLength := len(header)
-
-	// To determine the row number of an invalid row, we need to account for
-	// the header in the file
-	const headerOffset = 2
-
-	// Skip the header. Go through each row in the file checking that for each
-	// row there are no empty columns
-	for rowIndex, record := range uploadedCSV[1:] {
-		currentRecord := invalidRecord{}
-		currentRecord.RowNumber = rowIndex + headerOffset
-		recordIsValid := true
-
-		for columnIndex, field := range record {
-			if strings.TrimSpace(field) == "" {
-				recordIsValid = false
-				currentRecord.Columns = append(currentRecord.Columns, header[columnIndex])
-			}
-		}
-
-		if recordIsValid {
-			validRecords = append(validRecords, record)
-		} else if !recordIsValid && len(currentRecord.Columns) != headerLength {
-			invalidRecords = append(invalidRecords, currentRecord)
-		}
-	}
-	return validRecords, invalidRecords, nil
 }
