d284d130637d6fd39f9cce41ecd2cf0ffab974aa
Nana Osei Yeboah <yeboahnanaosei@gmail.com>
2020-04-19 11:23:27 +0000

    Make jsonResponse struct adhere to our JSON spec

--- main.go
+++ main.go
@@ -9,10 +9,10 @@ import (
 )
 
 type jsonResponse struct {
-	Success bool              `json:"success"`
-	Summary string            `json:"summary"`
-	Data    interface{}       `json:"data,omitempty"`
-	Error   map[string]string `json:"error,omitempty"`
+	Ok    bool              `json:"ok"`
+	Msg   string            `json:"msg"`
+	Data  interface{}       `json:"data,omitempty"`
+	Error map[string]string `json:"error,omitempty"`
 }
 
 func main() {
@@ -20,8 +20,8 @@ func main() {
 
 	// Make sure this program is called with exactly one argument
 	if len(os.Args) != 2 {
-		payload.Success = false
-		payload.Summary = "Exactly one argument expected"
+		payload.Ok = false
+		payload.Msg = "Exactly one argument expected"
 		payload.Error = map[string]string{
 			"msg": "cvet expects exactly one argument which is the path to the csv file being vetted",
 			"fix": fmt.Sprintf("call cvet with the path to the csv file as the first argument. Eg %s /path/to/csv/file", os.Args[0]),
@@ -32,8 +32,8 @@ func main() {
 
 	csvFile, err := os.Open(os.Args[1])
 	if err != nil {
-		payload.Success = false
-		payload.Summary = "An internal error occured"
+		payload.Ok = false
+		payload.Msg = "An internal error occured"
 		payload.Error = map[string]string{
 			"msg": fmt.Sprintf("There was an error trying to open the csv file: %v", err),
 			"fix": "Ensure you provided a valid csv file.",
@@ -47,8 +47,8 @@ func main() {
 	validRecords, invalidRecords, err := cval.Validate(csvFile)
 
 	if err != nil {
-		payload.Success = false
-		payload.Summary = "An internal error occured"
+		payload.Ok = false
+		payload.Msg = "An internal error occured"
 		payload.Error = map[string]string{
 			"msg": fmt.Sprintf("There was an error trying to process the csv file: %v", err),
 			"fix": "Ensure you provided a valid csv file. If this continues, please wait and try again later. You can also contact support",
@@ -57,8 +57,8 @@ func main() {
 		return
 	}
 
-	payload.Success = true
-	payload.Summary = "File vetted successfully"
+	payload.Ok = true
+	payload.Msg = "File vetted successfully"
 	payload.Data = map[string]interface{}{
 		"validRecords":   validRecords,
 		"invalidRecords": invalidRecords,
